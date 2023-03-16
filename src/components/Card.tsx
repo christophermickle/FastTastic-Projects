79a1c99da52ca886942de2d383e274cb0525be53
Christopher Mickle <cd.mickle@yahoo.com>
2023-03-16 15:56:32 -0700

    Worked for a while before this -initial commit

---
+++ src/components/Card.tsx
@@ -0,0 +1,35 @@
+import { Component } from "solid-js";
+import { Props } from "astro";
+
+interface Card {
+  thumbnail: string;
+  alt:string;
+  projectTitle: string;
+  projectDescription: string;
+  route: string;
+}
+
+export const Card: Component<Card> = (props) => {
+  return (
+    <>
+      <a
+        href={`${props.route}`}
+        class='
+        block
+        bg-white
+        p-4
+        rounded-lg
+        shadow-lg
+      '
+      >
+        <h2 class='text-lg font-semibold mb-2 text-center'>
+          {props.projectTitle}
+        </h2>
+
+        <p class='text-gray-600'>{props.projectDescription}</p>
+
+        <div>{props.thumbnail}</div>
+      </a>
+    </>
+  );
+};
---
+++ src/data/projects.ts
@@ -0,0 +1,76 @@
+export const projects = [
+  {
+    thumbnail: "",
+    projectTitle: "Dad Jokes Generator",
+    projectDescription: "",
+    route: "",
+  },
+  {
+    thumbnail: "",
+    projectTitle: "Feedback UI",
+    projectDescription: "",
+    route: "",
+  },
+  {
+    thumbnail: "",
+    projectTitle: "English Dictionary",
+    projectDescription: "",
+    route: "",
+  },
+  {
+    thumbnail: "",
+    projectTitle: "Random Quote Generator",
+    projectDescription: "",
+    route: "",
+  },
+  {
+    thumbnail: "",
+    projectTitle: "Anime Pics Generator",
+    projectDescription: "",
+    route: "",
+  },
+  {
+    thumbnail: "",
+    projectTitle: "Random Emoji",
+    projectDescription: "",
+    route: "",
+  },
+  {
+    thumbnail: "",
+    projectTitle: "Temperature Converter",
+    projectDescription: "",
+    route: "",
+  },
+  {
+    thumbnail: "",
+    projectTitle: "BMI Calculator",
+    projectDescription: "",
+    route: "",
+  },
+  {
+    thumbnail: "",
+    projectTitle: "Note App",
+    projectDescription: "",
+    route: "",
+  },
+  {
+    thumbnail: "",
+    projectTitle: "Currency Converter",
+    projectDescription: "",
+    route: "",
+  },
+  {
+    thumbnail: "",
+    projectTitle: "Loan Calculator",
+    projectDescription: "",
+    route: "",
+  },
+  {
+    thumbnail: "",
+    projectTitle: "Weight Converter",
+    projectDescription: "",
+    route: "",
+  },
+];
+
+// Later, you can use the `map` function to render the Card components with the project data.
